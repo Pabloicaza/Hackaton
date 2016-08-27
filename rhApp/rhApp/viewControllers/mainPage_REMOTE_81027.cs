d6209c54ef6cbb436f056db6d6d8f50180795359
Pabloicaza <pablo_icaza@hotmail.com>
2016-08-27 18:34:57 -0500

    Actualizacion

---
+++ rhApp/rhApp/viewControllers/mainPage_REMOTE_81027.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace rhApp
+{
+	public class mainPage : ContentPage
+	{
+		public mainPage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello Pablo" }
+				}
+			};
+		}
+	}
+}
+
+
