66057994b3e04dc4c1920c0949c2b3f14cb0e06d
jmenendez <josea_m95@hotmail.com>
2016-08-27 19:01:39 -0500

    Le agrege un espacio

--- rhApp/rhApp/viewControllers/mainPage.cs
+++ rhApp/rhApp/viewControllers/mainPage.cs
@@ -11,7 +11,7 @@ namespace rhApp
 			Content = new StackLayout
 			{
 				Children = {
-					new Label { Text = "Hello Wulakakaka" }
+					new Label { Text = "Hello  new wakalaka" }
 				}
 			};
 		}
